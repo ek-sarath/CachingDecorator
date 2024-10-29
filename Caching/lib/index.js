78cfc5275928ddf39860a0fa2d360231b2e224ff
ek-sarath <sarath.e@edstem.com>
2024-10-29 16:55:43 +0530

    Committed

--- Caching/lib/index.js
+++ Caching/lib/index.js
@@ -1,3 +1,101 @@
 "use strict";
-const k = 12;
-console.log(k);
+var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
+    var useValue = arguments.length > 2;
+    for (var i = 0; i < initializers.length; i++) {
+        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
+    }
+    return useValue ? value : void 0;
+};
+var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
+    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
+    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
+    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
+    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
+    var _, done = false;
+    for (var i = decorators.length - 1; i >= 0; i--) {
+        var context = {};
+        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
+        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
+        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
+        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
+        if (kind === "accessor") {
+            if (result === void 0) continue;
+            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
+            if (_ = accept(result.get)) descriptor.get = _;
+            if (_ = accept(result.set)) descriptor.set = _;
+            if (_ = accept(result.init)) initializers.unshift(_);
+        }
+        else if (_ = accept(result)) {
+            if (kind === "field") initializers.unshift(_);
+            else descriptor[key] = _;
+        }
+    }
+    if (target) Object.defineProperty(target, contextIn.name, descriptor);
+    done = true;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function Cached(options) {
+    return function (target, propertyKey, descriptor) {
+        const originalMethod = target[propertyKey];
+        target[propertyKey] = function (...args) {
+            const key = options.keyGenerator ? options.keyGenerator.apply(this, args) : propertyKey;
+            if (options.condition && !options.condition.apply(this, args)) {
+                return originalMethod.apply(this, args);
+            }
+            const cachedValue = localStorage.getItem(key);
+            if (cachedValue) {
+                return JSON.parse(cachedValue);
+            }
+            const result = originalMethod.apply(this, args);
+            localStorage.setItem(key, JSON.stringify(result));
+            return result;
+        };
+    };
+}
+let UserService = (() => {
+    var _a;
+    let _instanceExtraInitializers = [];
+    let _getUser_decorators;
+    return _a = class UserService {
+            getCurrentOrg() {
+                return '1';
+            }
+            isEnabled() {
+                return true;
+            }
+            getUser(id) {
+                return __awaiter(this, void 0, void 0, function* () {
+                    yield new Promise(resolve => setTimeout(resolve, 250));
+                    return { id, name: `User ${id}` };
+                });
+            }
+            constructor() {
+                __runInitializers(this, _instanceExtraInitializers);
+            }
+        },
+        (() => {
+            const _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+            _getUser_decorators = [Cached({
+                    ttl: 30000,
+                    keyGenerator: (id) => `user-${id}`,
+                    dependencies: function (id) {
+                        return [`org:${this.getCurrentOrg()}`, `user:${id}`];
+                    },
+                    condition: function (id) {
+                        return this.isEnabled();
+                    }
+                })];
+            __esDecorate(_a, null, _getUser_decorators, { kind: "method", name: "getUser", static: false, private: false, access: { has: obj => "getUser" in obj, get: obj => obj.getUser }, metadata: _metadata }, null, _instanceExtraInitializers);
+            if (_metadata) Object.defineProperty(_a, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+        })(),
+        _a;
+})();
+console.log(new UserService().getUser('1').then(user => console.log(user)));
--- Caching/src/index.ts
+++ Caching/src/index.ts
@@ -1,2 +1,65 @@
-const k = 12
-console.log(k);
+interface CacheOptions<T extends object> {
+    ttl?: number;
+    keyGenerator?: (...args: any[]) => string;
+    dependencies?: (this: T, ...args: any[]) => string[]; 
+    condition?: (this: T, ...args: any[]) => boolean;
+}
+
+interface User {
+    id: string;
+    name: string;
+}
+
+function Cached<T extends object>(
+    options: CacheOptions<T>
+) {
+    return function (
+        target: T,
+        propertyKey: string,
+        descriptor: PropertyDescriptor
+    )
+    {
+        const originalMethod = target[propertyKey];
+        target[propertyKey] = function (...args: any[]) {
+            const key = options.keyGenerator ? options.keyGenerator.apply(this, args) : propertyKey;
+            if (options.condition && !options.condition.apply(this, args)) {
+                return originalMethod.apply(this, args);
+            }
+            const cachedValue = localStorage.getItem(key);
+            if (cachedValue) {
+                return JSON.parse(cachedValue);
+            }
+            const result = originalMethod.apply(this, args);
+            localStorage.setItem(key, JSON.stringify(result));
+            return result;
+        };
+    };
+}
+
+class UserService {
+    getCurrentOrg(): string {
+        return '1';
+    }
+    isEnabled(): boolean {
+        return true;
+    }
+
+    @Cached<UserService>({
+        ttl: 30000,
+        keyGenerator: (id: string) => `user-${id}`,
+        dependencies: function (id : string) {
+            return[`org:${this.getCurrentOrg()}`, `user:${id}`];
+        },
+        condition: function (id: string) {
+            return this.isEnabled();
+        }
+    })
+    async getUser(id: string): Promise<User> {
+        await new Promise(resolve => setTimeout(resolve, 250));
+        return { id, name: `User ${id}` };
+    }
+}
+
+console.log(
+    new UserService().getUser('1').then(user => console.log(user))
+);
